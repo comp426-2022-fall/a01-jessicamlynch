72724d4a8150a1b92ebfc084651c7e4801605532
Jessica Lynch <jess@sparkie.io>
2022-09-01 09:05:54 -0400

    first changes

--- server.js
+++ server.js
@@ -1,13 +1,20 @@
 // Require http module
+   const http = require('http');
 
 // Require fs module
+   const fs = require('fs');
 
 // Require minimist module (make sure you install this one via npm).
+   const args = require('minimist')(process.argv.slice(2));
 
 // Use minimist to process one argument `--port=` on the command line after `node server.js`.
+   console.log(args); 
+   // in command line: node server.js -a beep -b boop 
+   // result: { _: [], a: 'beep', b: 'boop' }
 
 // Define a const `port` using the argument from the command line. 
 // Make this const default to port 3000 if there is no argument given for `--port`.
+   const port = args.port || 3000;
 
 // Use the fs module to create an arrow function using `fs.readFile`.
 // Use the documentation for the Node.js `fs` module. 
